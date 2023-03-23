060917ba47deceef706524c8610f8853e265eee7
Anuppaswan24 <119486812+Anuppaswan24@users.noreply.github.com>
2023-03-23 09:54:53 -0700

    Add files via upload

---
+++ task5/dom.js
@@ -0,0 +1,13 @@
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+// console.log(items[3]);
+items[0].style.fontWeight='bold';
+items[1].style.fontWeight='bold';
+items[2].style.fontWeight='bold';
+items[3].style.fontWeight='bold';
+// console.log(items);
+// console.log(items[2]);
+items[2].style.backgroundColor = 'red';
+var item = document.getElementsByClassName('ItemAnup');
+item[0].style.backgroundColor = 'yellow';
+var item = document.getElementsByClassName('ItemAnup');
